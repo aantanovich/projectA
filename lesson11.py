ade9aa5ccaea9a2b69b0fb00d023e3a4a07b53f5
Anton <antanovich@gmail.com>
2020-07-02 18:57:50 -0400

    commit numero 2

---
+++ lesson11.py
@@ -0,0 +1,14 @@
+
+
+def print_lyrics():
+    print("sing a song.")
+    print("now i song a sing")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    print_lyrics()
+
+
+repeat_lyrics()
+print("fin")
